1412ba78552ee3b365fa972cc9fc9c290fecc44d
dzhumadil <dzhumadil.a@icloud.com>
2023-04-08 19:06:08 +0600

    Initial Commit

---
+++ lesson1.4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lesson1.4
+//
+//  Created by Абдулла-Бек on 8/4/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
